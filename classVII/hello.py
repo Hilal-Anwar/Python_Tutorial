c12f46c8558a76dd3d776677b3035b4c9ebdad16
hilal <hilal123anwar@gmail.com>
2024-05-11 21:47:28 +0530

    Added new questions

---
+++ classVII/hello.py
@@ -0,0 +1,12 @@
+print('Hello world ! from class 7th side', end='')
+print('I study in graded classes')
+print(5 + 6)
+print(57878 * 687854)
+print(7 / 2)
+print(5 ** 3)
+a = 20
+b = 30
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
